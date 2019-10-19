e12d4a127c194238148fda3bbe9d200bdd775ec7
Vasili Platonau <wert666@bk.ru>
2019-10-19 10:14:29 +0300

    Initial commit

---
+++ src/main/java/NumbersSumMulti.java
@@ -0,0 +1,19 @@
+public class NumbersSumMulti {
+
+    public static void main(String[] args) {
+        int sum=0;
+        int mult=1;
+        for (int i=0; i<args.length;i++){
+            int num = Integer.parseInt(args[i]);
+            if (num%1==0) {
+                sum+=num;
+                mult*=num;
+            }else {
+                System.out.println("Number not Integer");
+            }
+
+        }
+        System.out.println("Sum: "+ sum);
+        System.out.println("Multiplication: " + mult);
+    }
+}
